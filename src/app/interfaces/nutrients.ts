61d5e731ee47903b9facd95954572a9ddcf5464e
jakeb-k <jknow30@outlook.com>
2024-01-15 09:03:06 +1000

    nutrient interface init

---
+++ src/app/interfaces/nutrients.ts
@@ -0,0 +1,32 @@
+export interface Nutrients {
+    calories: number;
+    fat: number;
+    saturatedFat: number;
+    carbohydrates: number;
+    netCarbohydrates: number;
+    sugar: number;
+    cholesterol: number;
+    sodium: number;
+    protein: number;
+    vitaminC: number;
+    vitaminB3: number;
+    vitaminB6: number;
+    selenium: number;
+    vitaminB2: number;
+    phosphorus: number;
+    manganese: number;
+    potassium: number;
+    vitaminB1: number;
+    folate: number;
+    iron: number;
+    vitaminB5: number;
+    zinc: number;
+    magnesium: number;
+    vitaminB12: number; // Assuming 'Vitamin' refers to Vitamin D
+    copper: number;
+    vitaminA: number;
+    calcium: number;
+    fiber: number;
+    vitaminE: number;
+    vitaminK: number;
+}
