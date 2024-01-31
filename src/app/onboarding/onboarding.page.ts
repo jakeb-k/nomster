eb3b424584164fc13d64359d905d21ca731b4b59
jakeb-k <jknow30@outlook.com>
2024-01-31 14:43:36 +1000

    onboarding form

--- src/app/onboarding/onboarding.page.ts
+++ src/app/onboarding/onboarding.page.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OnboardingPage implements OnInit {
 
+  icons = [
+    { label: 'Loss', value: '-1', name: 'caret-up-outline' },
+    { label: 'Maintain', value: '0', name: 'checkmark-outline' },
+    { label: 'Gain', value: '1', name: 'caret-down-outline' },
+  ];
+
+  selectedIcon: string | null = null;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  selectIcon(value: string) {
+    this.selectedIcon = value;
+  }
 }
