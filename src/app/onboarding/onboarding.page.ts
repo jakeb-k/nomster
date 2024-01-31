5d3fc47eecd5009618f05e7058525de54dc1396c
jakeb-k <jknow30@outlook.com>
2024-01-31 15:22:04 +1000

    datetime onboarding

--- src/app/onboarding/onboarding.page.ts
+++ src/app/onboarding/onboarding.page.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OnboardingPage implements OnInit {
 
+  dateInput!: string; 
+
   icons = [
     { label: 'Loss', value: '-1', name: 'caret-up-outline' },
     { label: 'Maintain', value: '0', name: 'checkmark-outline' },
@@ -22,5 +24,16 @@ export class OnboardingPage implements OnInit {
 
   selectIcon(value: string) {
     this.selectedIcon = value;
+  }
+
+  formatDate(){
+    const dateRegex = /^(0[1-9]|1[0-2])\/(0[1-9]|[12][0-9]|3[01])\/\d{2}$/;
+
+    // Check if the current input matches the format
+    if (!this.dateInput.match(dateRegex)) {
+      // If not, implement logic to reformat or notify the user
+      console.log('Invalid date format. Please use MM/DD/YY.');
+      // You can also implement logic to auto-correct the input here
+    }
   }
 }
