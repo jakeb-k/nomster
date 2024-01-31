e957a92d34d7992263d1fccfee2c414e4bbae21a
jakeb-k <jknow30@outlook.com>
2024-01-31 15:55:51 +1000

    onboarding form frontend

--- src/app/onboarding/onboarding.page.ts
+++ src/app/onboarding/onboarding.page.ts
@@ -9,13 +9,27 @@ export class OnboardingPage implements OnInit {
 
   dateInput!: string; 
 
-  icons = [
-    { label: 'Loss', value: '-1', name: 'caret-up-outline' },
-    { label: 'Maintain', value: '0', name: 'checkmark-outline' },
-    { label: 'Gain', value: '1', name: 'caret-down-outline' },
+  gIcons = [
+    { label: 'Man', value: '0', name: 'woman-outline' },
+    { label: 'Woman', value: '1', name: 'man-outline' },
   ];
 
+  wIcons = [
+    { label: 'Loss', value: '-1', name: 'caret-down-outline' },
+    { label: 'Maintain', value: '0', name: 'checkmark-outline' },
+    { label: 'Gain', value: '1', name: 'caret-up-outline' },
+  ];
+
+  activityLevels = [
+    { level: "Sedentary: little or no exercise", factor: 1.2 },
+    { level: "Lightly active: exercise 1-3 times a week", factor: 1.375 },
+    { level: "Moderately active: exercise 4-5 times a week", factor: 1.55 },
+    { level: "Very active: daily exercise", factor: 1.725 },
+    { level: "Super active: very physical job or 2x daily exercise", factor: 1.9 }
+  ]; 
+
   selectedIcon: string | null = null;
+  selectedGenderIcon: string | null = null; 
 
   constructor() { }
 
@@ -25,15 +39,8 @@ export class OnboardingPage implements OnInit {
   selectIcon(value: string) {
     this.selectedIcon = value;
   }
-
-  formatDate(){
-    const dateRegex = /^(0[1-9]|1[0-2])\/(0[1-9]|[12][0-9]|3[01])\/\d{2}$/;
-
-    // Check if the current input matches the format
-    if (!this.dateInput.match(dateRegex)) {
-      // If not, implement logic to reformat or notify the user
-      console.log('Invalid date format. Please use MM/DD/YY.');
-      // You can also implement logic to auto-correct the input here
-    }
+  selectIcon2(value: string) {
+    this.selectedGenderIcon = value;
   }
+
 }
