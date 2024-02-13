b3b42cb2784ef2bfc8bc8d85ab2bea31588aed1b
jakeb-k <jknow30@outlook.com>
2024-02-13 12:15:24 +1000

    time init check

--- src/app/services/reset.service.ts
+++ src/app/services/reset.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { UserService } from './user.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ResetService {
+  user:any; 
+  timeStamp: any; 
+  constructor(private userService: UserService) {
 
-  constructor() { }
+   }
+   
+   async timeCheck() {
+
+    this.user = this.userService.loadUserByPromise();
+    this.timeStamp = this.user.timeStamp;
+    console.log("timestamp: ", this.timeStamp) 
+    const now = new Date().getTime();
+
+    // Check if more than 24 hours have passed
+    if (now - this.timeStamp > 60 * 1000) {
+      console.log('its been a minute')
+    } else {
+      console.log('it has not been a minute')
+    }
+
+   }
 }
