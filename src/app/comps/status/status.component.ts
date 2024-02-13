6aba30eee558ad3f0cfcaf8c4c9cab0e5e1893e4
jakeb-k <jknow30@outlook.com>
2024-02-13 16:07:25 +1000

    status html n styling

--- src/app/comps/status/status.component.ts
+++ src/app/comps/status/status.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-status',
@@ -6,9 +6,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./status.component.scss'],
 })
 export class StatusComponent  implements OnInit {
+  // Input property for a favourite item
+  @Input() props = {
+    status: Boolean(), 
+    message: "",
+  };
+  styles = {
+    icon: "",
+    iconColor: "",
+    color: "",
+    bg: "",
+  }
+  
 
   constructor() { }
 
   ngOnInit() {}
 
+  updateCompByStatus() {
+    if(this.props.status) {
+      this.styles.icon = "thumbs-up-outline"
+      this.styles.iconColor = "success"
+      this.styles.color = "#3c763d"
+      this.styles.bg = "#dff0d8"
+    } else {
+      this.styles.icon = "thumbs-down-outline"
+      this.styles.iconColor = "danger"
+      this.styles.color = "#763b3b"
+      this.styles.bg = "#f0d7d7"
+    }
+    
+  }
 }
--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from 'src/app/comps/header/header.component';
 import { UpdateGoalModalComponent } from 'src/app/comps/update-goal-modal/update-goal-modal.component';
+import { StatusComponent } from 'src/app/comps/status/status.component';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
-  declarations: [HeaderComponent, UpdateGoalModalComponent],
+  declarations: [HeaderComponent, UpdateGoalModalComponent, StatusComponent],
   imports: [
     CommonModule,
     IonicModule,
