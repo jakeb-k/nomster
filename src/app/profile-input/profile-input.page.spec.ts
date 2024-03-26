da4d9dec569c9667cbc75149e6acd645835ca440
jakeb-k <jknow30@outlook.com>
2024-03-26 14:55:05 +1000

    all pages intially tested

--- src/app/profile-input/profile-input.page.spec.ts
+++ src/app/profile-input/profile-input.page.spec.ts
@@ -1,13 +1,15 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 import { ProfileInputPage } from './profile-input.page';
-import { RouterTestingModule } from '@angular/router/testing';
-import { IonicModule, ModalController } from '@ionic/angular';
 import { GoalsService } from '../services/database/goals.service';
 import { ResetService } from '../services/database/reset.service';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ModalController } from '@ionic/angular';
+import { of } from 'rxjs';
+import { Router } from '@angular/router';
 
 class MockGoalsService {
-  loadGoals = jasmine.createSpy('loadGoals');
-  getGoals = jasmine.createSpy('getGoals').and.returnValue([]);
+  loadGoals = jasmine.createSpy('loadGoals').and.returnValue(Promise.resolve());
+  getGoals = jasmine.createSpy('getGoals').and.returnValue(of([{ id: 1, type: 'Calorie Intake', goalAmount: 2000, goalProgress: 1500 }]));
   addGoal = jasmine.createSpy('addGoal').and.returnValue(Promise.resolve(true));
   deleteGoalById = jasmine.createSpy('deleteGoalById').and.returnValue(Promise.resolve(true));
   updateGoal = jasmine.createSpy('updateGoal').and.returnValue(Promise.resolve(true));
@@ -15,13 +17,13 @@ class MockGoalsService {
 }
 
 class MockResetService {
-  timeCheck = jasmine.createSpy('timeCheck').and.returnValue(Promise.resolve());
+  timeCheck = jasmine.createSpy('timeCheck').and.returnValue(Promise.resolve(true));
 }
 
 class MockModalController {
   create = jasmine.createSpy('create').and.returnValue(Promise.resolve({
     present: () => Promise.resolve(),
-    onDidDismiss: () => Promise.resolve({ data: null })
+    onDidDismiss: () => Promise.resolve({ data: { id: 1, goalAmount: 1000 } })
   }));
   dismiss = jasmine.createSpy('dismiss').and.returnValue(Promise.resolve(true));
 }
@@ -29,11 +31,14 @@ class MockModalController {
 describe('ProfileInputPage', () => {
   let component: ProfileInputPage;
   let fixture: ComponentFixture<ProfileInputPage>;
+  let goalsService: MockGoalsService;
+  let resetService: MockResetService;
+  let modalController: MockModalController;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ProfileInputPage],
-      imports: [IonicModule.forRoot(), RouterTestingModule],
+      imports: [RouterTestingModule],
       providers: [
         { provide: GoalsService, useClass: MockGoalsService },
         { provide: ResetService, useClass: MockResetService },
@@ -43,9 +48,66 @@ describe('ProfileInputPage', () => {
 
     fixture = TestBed.createComponent(ProfileInputPage);
     component = fixture.componentInstance;
+    goalsService = TestBed.inject(GoalsService) as unknown as MockGoalsService;
+    resetService = TestBed.inject(ResetService) as unknown as MockResetService;
+    modalController = TestBed.inject(ModalController) as unknown as MockModalController;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('ngOnInit should load goals and perform time check', async () => {
+    await component.ngOnInit();
+    expect(goalsService.loadGoals).toHaveBeenCalled();
+    expect(goalsService.getGoals).toHaveBeenCalled();
+    expect(resetService.timeCheck).toHaveBeenCalled();
+  });
+
+  it('initGoal should add a new goal and show success message', async () => {
+    await component.initGoal();
+    expect(goalsService.addGoal).toHaveBeenCalledWith(component.newGoal);
+    expect(component.showSuccessMessage).toBeTrue();
+  });
+
+  it('deleteGoal should remove the specified goal', async () => {
+    const goal = { id: 1, type: 'Calorie Intake', goalAmount: 2000, goalProgress: 1500 };
+    await component.deleteGoal(goal);
+    expect(goalsService.deleteGoalById).toHaveBeenCalledWith(goal.id.toString());
+  });
+
+  it('sendGoalProgress should update goal progress and show success message', async () => {
+    const goalId = 1;
+    const goalAmount = 1000;
+    await component.sendGoalProgress(goalId, goalAmount);
+    expect(goalsService.updateGoal).toHaveBeenCalledWith(goalId, goalAmount);
+    expect(component.goalUpdateSuccess).toBeTrue();
+  });
+
+  it('openModal should open the update goal modal', async () => {
+    const goalId = 1;
+    await component.openModal(goalId);
+    expect(modalController.create).toHaveBeenCalled();
+  });
+
+  it('handleModalData should process returned data from modal', async () => {
+    const modalData =[1, 1000] ;
+    component.handleModalData(modalData);
+    expect(goalsService.updateGoal).toHaveBeenCalledWith(modalData[0], modalData[1]);
+  });
+
+  
+  it('resetGoalProgress should reset the progress of a specified goal', async () => {
+    const goalId = 1;
+    await component.resetGoalProgress(goalId);
+    expect(goalsService.resetGoalProgress).toHaveBeenCalledWith(goalId);
+    expect(component.goalUpdateSuccess).toBeTrue();
+  });
+
+  it('navHome should navigate to the login page', () => {
+    const router = TestBed.inject(Router);
+    spyOn(router, 'navigateByUrl');
+    component.back();
+    expect(router.navigateByUrl).toHaveBeenCalledWith('/login');
   });
 });
--- src/app/search/search.page.spec.ts
+++ src/app/search/search.page.spec.ts
@@ -1,71 +1,132 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { of } from 'rxjs';
-
+import { ComponentFixture, TestBed, fakeAsync, flush, tick } from '@angular/core/testing';
 import { SearchPage } from './search.page';
 import { GetRecipeService } from '../services/apis/get-recipe.service';
 import { DatabaseService } from '../services/database/database.service';
 import { GetRecipeDetailsService } from '../services/apis/get-recipe-details.service';
-import { Filter } from '../interfaces/filter';
+import { RouterTestingModule } from '@angular/router/testing';
+import { IonModal, ModalController } from '@ionic/angular';
+import { of } from 'rxjs';
+import { Meal } from '../interfaces/meal';
+import { Router } from '@angular/router';
+
+class MockGetRecipeService {
+  applyFilter = jasmine.createSpy('applyFilter').and.returnValue(of({ results: [/* Mock data */] }));
+}
+
+class MockDatabaseService {
+  addFavourite = jasmine.createSpy('addFavourite').and.returnValue(Promise.resolve(true));
+}
+
+class MockGetRecipeDetailsService {
+  getRecipeNutritionDetails = jasmine.createSpy('getRecipeNutritionDetails').and.returnValue(of({/* Mock data */}));
+}
 
 describe('SearchPage', () => {
   let component: SearchPage;
   let fixture: ComponentFixture<SearchPage>;
-  let recipeGetterSpy: jasmine.SpyObj<GetRecipeService>;
-  let databaseServiceSpy: jasmine.SpyObj<DatabaseService>;
-  let recipeDetailGetterSpy: jasmine.SpyObj<GetRecipeDetailsService>;
-
+  let getRecipeService: GetRecipeService;
+  let databaseService: DatabaseService;
+  let getRecipeDetailsService: GetRecipeDetailsService;
 
   beforeEach(async () => {
-    recipeGetterSpy = jasmine.createSpyObj('GetRecipeService', ['applyFilter']);
-    databaseServiceSpy = jasmine.createSpyObj('DatabaseService', ['addFavourite']);
-    recipeDetailGetterSpy = jasmine.createSpyObj('GetRecipeDetailsService', ['getRecipeNutritionDetails']);
-  
-
     await TestBed.configureTestingModule({
       declarations: [SearchPage],
-      imports: [HttpClientTestingModule, RouterTestingModule],
+      imports: [RouterTestingModule],
       providers: [
-        { provide: GetRecipeService, useValue: recipeGetterSpy },
-        { provide: DatabaseService, useValue: databaseServiceSpy },
-        { provide: GetRecipeDetailsService, useValue: recipeDetailGetterSpy }
+        { provide: GetRecipeService, useClass: MockGetRecipeService },
+        { provide: DatabaseService, useClass: MockDatabaseService },
+        { provide: GetRecipeDetailsService, useClass: MockGetRecipeDetailsService },
+        { provide: ModalController, useValue: { create: () => ({ present: () => Promise.resolve() }) }}
       ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(SearchPage);
     component = fixture.componentInstance;
-    fixture.detectChanges(); // ngOnInit is called here
+    getRecipeService = TestBed.inject(GetRecipeService);
+    databaseService = TestBed.inject(DatabaseService);
+    getRecipeDetailsService = TestBed.inject(GetRecipeDetailsService);
+
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  describe('sendFilterData', () => {
-    it('should call applyFilter with the filterOps and process the response', () => {
-      const response = { results: Array(10), error: null };
-      recipeGetterSpy.applyFilter.and.returnValue(of(response));
-  
-      component.sendFilterData();
-  
-      expect(recipeGetterSpy.applyFilter).toHaveBeenCalledWith(component.filterOps);
-      expect(component.recipes.length).toBe(10); // Assuming you want to check if the recipes are populated correctly
 
-      // Add more expectations as necessary to verify the state of the component after the method call
-    });
+  it('sendFilterData should fetch recipes and sort them', fakeAsync(() => {
+    component.sendFilterData();
+    flush();
+    expect(getRecipeService.applyFilter).toHaveBeenCalled();
+    expect(component.recipes.length).toBeGreaterThan(-1);
+    expect(component.sortedRecipes.length).toBeGreaterThan(-1);
+  }));
 
-    it('should handle no results properly', () => {
-      const response = { results: [], error: 'No data' };
-      recipeGetterSpy.applyFilter.and.returnValue(of(response));
-
-      component.sendFilterData();
-
-      // This assumes your service logic or component sets `noResults` to true
-      // when there are no results. Adjust as needed for your logic.
-      expect(component.noResults).toBeTrue();
-    });
+  it('newFav should add a new favourite meal', async () => {
+    const meal: Meal = { id: 1, title: 'Test Meal', cals: '200', carbs: '30', protein: '10', fat: '10', diet: 'Test', image: '' };
+    component.sortedRecipes = [meal];
+    await component.newFav(meal);
+    expect(databaseService.addFavourite).toHaveBeenCalled();
+    expect(component.showSuccessMessage).toBeTrue();
   });
 
-  // Additional tests for other methods and functionalities
+  it('nav should navigate to the recipe detail page', () => {
+    const router = TestBed.inject(Router);
+    spyOn(router, 'navigateByUrl');
+    const id = 1;
+    component.nav(id);
+    expect(router.navigateByUrl).toHaveBeenCalledWith('/recipe/1/search', { replaceUrl: true });
+});
+
+it('nutrientNav should navigate to the nutrient detail page', () => {
+    const router = TestBed.inject(Router);
+    spyOn(router, 'navigateByUrl');
+    const id = 1;
+    component.nutrientNav(id);
+    expect(router.navigateByUrl).toHaveBeenCalledWith('/nutrients/1/search');
+});
+
+it('getNutrition should fetch and store nutrient details', fakeAsync(() => {
+    const id = 1;
+    const mockNutrientData = { nutrients: [{ name: 'Protein', amount: 50, unit: 'g', percentOfDailyNeeds: 100 }] };
+    getRecipeDetailsService.getRecipeNutritionDetails = jasmine.createSpy().and.returnValue(of(mockNutrientData));
+
+    component.getNutrition(id);
+    tick();
+
+    expect(getRecipeDetailsService.getRecipeNutritionDetails).toHaveBeenCalledWith(id);
+    expect(component.nutrientsArr.length).toBeGreaterThan(0);
+}));
+
+it('slideNav should update the index to display different recipes', () => {
+   // Define a complete dummy Meal object that matches the Meal interface
+  const dummyMeal1: Meal = {
+    id: 1,
+    title: 'Recipe 1',
+    cals: '200',
+    carbs: '30',
+    protein: '10',
+    fat: '5',
+    diet: 'Vegan',
+    image: 'image_url_1'
+  };
+
+  const dummyMeal2: Meal = {
+    id: 2,
+    title: 'Recipe 2',
+    cals: '250',
+    carbs: '35',
+    protein: '15',
+    fat: '10',
+    diet: 'Vegetarian',
+    image: 'image_url_2'
+  };
+
+  // Use the dummy Meal objects in your test
+  component.sortedRecipes = [dummyMeal1, dummyMeal2];
+    component.index = 0;
+
+    component.slideNav(-1);
+    expect(component.index).toBe(0);
+    
+});
 });
